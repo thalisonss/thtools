fd105d784582dcaacce2248431164418f433405c
Thalison Santos <thalisondssantos@gmail.com>
2023-10-28 16:33:56 -0300

    (+) Adicionado funções para carregar os planos de energia no combobox; (+) Adicionado funções para aplicar o plano de energia.

---
+++ Screens/MainScreen.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ThTools.Screens
+{
+    partial class frmMainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboPowerPlan = new System.Windows.Forms.ComboBox();
+            this.btnAplicar = new System.Windows.Forms.Button();
+            this.lblPlanoAtivo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboPowerPlan
+            // 
+            this.cboPowerPlan.FormattingEnabled = true;
+            this.cboPowerPlan.Location = new System.Drawing.Point(12, 212);
+            this.cboPowerPlan.Name = "cboPowerPlan";
+            this.cboPowerPlan.Size = new System.Drawing.Size(697, 21);
+            this.cboPowerPlan.TabIndex = 0;
+            // 
+            // btnAplicar
+            // 
+            this.btnAplicar.Location = new System.Drawing.Point(628, 240);
+            this.btnAplicar.Name = "btnAplicar";
+            this.btnAplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnAplicar.TabIndex = 1;
+            this.btnAplicar.Text = "button1";
+            this.btnAplicar.UseVisualStyleBackColor = true;
+            this.btnAplicar.Click += new System.EventHandler(this.btnAplicar_Click);
+            // 
+            // lblPlanoAtivo
+            // 
+            this.lblPlanoAtivo.AutoSize = true;
+            this.lblPlanoAtivo.Location = new System.Drawing.Point(251, 124);
+            this.lblPlanoAtivo.Name = "lblPlanoAtivo";
+            this.lblPlanoAtivo.Size = new System.Drawing.Size(38, 13);
+            this.lblPlanoAtivo.TabIndex = 2;
+            this.lblPlanoAtivo.Text = "label1";
+            // 
+            // frmMainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.ClientSize = new System.Drawing.Size(733, 276);
+            this.Controls.Add(this.lblPlanoAtivo);
+            this.Controls.Add(this.btnAplicar);
+            this.Controls.Add(this.cboPowerPlan);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMainScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboPowerPlan;
+        private System.Windows.Forms.Button btnAplicar;
+        private System.Windows.Forms.Label lblPlanoAtivo;
+    }
+}
---
+++ Screens/MainScreen.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ThTools.Screens
+{
+    public partial class frmMainScreen : Form
+    { 
+        public frmMainScreen()
+        {
+            InitializeComponent();
+            CarregarPlanosEnergia();
+            AtualizarPlanoEnergiaAtivo();
+        }
+
+        #region | Modelo |
+        public class PlanoEnergia
+        {
+            public string Nome { get; set; }
+            public string Codigo { get; set; }
+
+            public override string ToString()
+            {
+                return Nome;
+            }
+        }
+        #endregion
+        
+        #region | Controles |
+        private void btnAplicar_Click(object sender, EventArgs e)
+        {
+            AplicarPlanoEnergiaSelecionado();
+
+        }
+        #endregion
+
+        #region | Funções |
+        private void CarregarPlanosEnergia()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c powercfg /l";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            string[] lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                Regex regex = new Regex(@"\b[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\b");
+                Match match = regex.Match(line);
+
+                if (match.Success)
+                {
+                    string codigo = match.Value;
+                    string nome = line.Replace(match.Value, "").Trim();
+
+                    // Remover parênteses e asterisco do nome
+                    nome = nome.Replace("(", "").Replace(")", "").Replace("GUID do Esquema de Energia:", "").Trim();
+
+                    PlanoEnergia plano = new PlanoEnergia
+                    {
+                        Nome = nome,
+                        Codigo = codigo
+                    };
+
+                    cboPowerPlan.Items.Add(plano);
+                }
+            }
+
+            if (cboPowerPlan.Items.Count > 0)
+            {
+                cboPowerPlan.SelectedIndex = 0;
+            }
+            else
+            {
+                cboPowerPlan.Items.Add("Nenhum plano de energia disponível");
+                cboPowerPlan.SelectedIndex = 0;
+            }
+        }
+
+        private void AplicarPlanoEnergiaSelecionado()
+        {
+            if (cboPowerPlan.SelectedItem != null)
+            {
+                PlanoEnergia planoSelecionado = (PlanoEnergia)cboPowerPlan.SelectedItem;
+                string codigo = planoSelecionado.Codigo;
+
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = $"/c powercfg -setactive {codigo}";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+                process.WaitForExit();
+                lblPlanoAtivo.Text = "Plano de Energia Ativo: " + planoSelecionado;
+
+            }
+            else
+            {
+                MessageBox.Show("Nenhum plano de energia selecionado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void AtualizarPlanoEnergiaAtivo()
+        {
+            string planoAtivo = "Nenhum plano de energia ativo";
+
+            foreach (PlanoEnergia plano in cboPowerPlan.Items)
+            {
+                if (plano.Nome.EndsWith("*"))
+                {
+                    // Remove o asterisco do nome antes de exibir
+                    planoAtivo = plano.Nome.TrimEnd('*').Trim();
+                    break;
+                }
+            }
+
+            lblPlanoAtivo.Text = "Plano de Energia Ativo: " + planoAtivo;
+        }
+        #endregion
+    }
+}
